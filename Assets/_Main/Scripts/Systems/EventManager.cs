b0338c9b4d4dc95cac9c3d7a96ae6679574e42cd
Tran Quang Huy <86909150+HuyTran2uang@users.noreply.github.com>
2024-08-27 11:52:34 +0700

    event manager

---
+++ Assets/_Main/Scripts/Systems/EventManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System;
+
+public static class EventManager
+{
+    private static Dictionary<string, Delegate> _eventDictionary = new Dictionary<string, Delegate>();
+
+    /// <summary>
+    /// example: Delegate del = new Action<int, float, bool>(MyFunc);
+    /// </summary>
+    /// <param name="eventName">assdasd</param>
+    /// <param name="listener"></param>
+    public static void StartListening(string eventName, Delegate listener)
+    {
+        if (_eventDictionary.TryGetValue(eventName, out Delegate thisEvent))
+        {
+            _eventDictionary[eventName] = Delegate.Combine(thisEvent, listener);
+        }
+        else
+        {
+            _eventDictionary.Add(eventName, listener);
+        }
+    }
+
+    public static void StopListening(string eventName, Delegate listener)
+    {
+        if (_eventDictionary.TryGetValue(eventName, out Delegate thisEvent))
+        {
+            _eventDictionary[eventName] = Delegate.Remove(thisEvent, listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName, params object[] parameters)
+    {
+        if (_eventDictionary.TryGetValue(eventName, out Delegate thisEvent))
+        {
+            thisEvent.DynamicInvoke(parameters);
+        }
+    }
+}
