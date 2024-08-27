5b92b175f1163640369c0011a72717afa298b528
Tran Quang Huy <86909150+HuyTran2uang@users.noreply.github.com>
2024-08-27 13:27:02 +0700

    state machine

---
+++ Assets/_Main/Scripts/Systems/Stat Machine/Example/Robot.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+namespace Assets._Main.Scripts.Systems.Stat_Machine.Example
+{
+    public class Robot : MonoBehaviour
+    {
+        private StateMachine<Robot> _stateMachine;
+
+        private void Start()
+        {
+            _stateMachine = new StateMachine<Robot>(this);
+            _stateMachine.ChangeState(new Idle());
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                ChangeState(new Move());
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ChangeState(new Attack());
+            }
+            else
+            {
+                ChangeState(new Idle());
+            }
+
+            _stateMachine.Update();
+        }
+
+        public void ChangeState(State<Robot> newState)
+        {
+            _stateMachine.ChangeState(newState);
+        }
+    }
+
+    public class Idle : State<Robot>
+    {
+        public override void EnterState(Robot owner)
+        {
+            Debug.Log("EnterState" + this.GetType());
+        }
+
+        public override void ExecuteState(Robot owner)
+        {
+            Debug.Log("ExecuteState" + this.GetType());
+        }
+
+        public override void ExitState(Robot owner)
+        {
+            Debug.Log("ExitState" + this.GetType());
+        }
+    }
+
+    public class Move : State<Robot>
+    {
+        public override void EnterState(Robot owner)
+        {
+            Debug.Log("EnterState" + this.GetType());
+        }
+
+        public override void ExecuteState(Robot owner)
+        {
+            Debug.Log("ExecuteState" + this.GetType());
+        }
+
+        public override void ExitState(Robot owner)
+        {
+            Debug.Log("ExitState" + this.GetType());
+        }
+    }
+
+    public class Attack : State<Robot>
+    {
+        public override void EnterState(Robot owner)
+        {
+            Debug.Log("EnterState" + this.GetType());
+        }
+
+        public override void ExecuteState(Robot owner)
+        {
+            Debug.Log("ExecuteState" + this.GetType());
+        }
+
+        public override void ExitState(Robot owner)
+        {
+            Debug.Log("ExitState" + this.GetType());
+        }
+    }
+}
---
+++ Assets/_Main/Scripts/Systems/Stat Machine/StateMachine.cs
@@ -0,0 +1,44 @@
+﻿public class StateMachine<T>
+{
+    private T _owner;
+    private State<T> _currentState;
+    private State<T> _previousState;
+    private State<T> _globalState;
+
+    public StateMachine(T owner)
+    {
+        _owner = owner;
+    }
+
+    public void ChangeState(State<T> newState)
+    {
+        _previousState = _currentState;
+        _currentState?.ExitState(_owner);
+        _currentState = newState;
+        _currentState?.EnterState(_owner);
+    }
+
+    public void Update()
+    {
+        _globalState?.ExecuteState(_owner);
+        _currentState?.ExecuteState(_owner);
+    }
+
+    public void RevertToPreviousState()
+    {
+        ChangeState(_previousState);
+    }
+
+    public void SetGlobalState(State<T> state)
+    {
+        _globalState = state;
+        _globalState?.EnterState(_owner);
+    }
+}
+
+public abstract class State<T>
+{
+    public abstract void EnterState(T owner);
+    public abstract void ExecuteState(T owner);
+    public abstract void ExitState(T owner);
+}
